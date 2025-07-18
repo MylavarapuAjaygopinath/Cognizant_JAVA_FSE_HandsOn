f2923954dd4e9ae1279be1807563c96f66a12658
MylavarapuAjaygopinath <mylavarapuajay49@gmail.com>
2025-07-18 19:00:14 +0530

    Add files via upload

---
+++ Week_5/loan/src/test/java/com/loan/LoanApplicationTest.java
@@ -0,0 +1,12 @@
+package com.loan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+
+}
